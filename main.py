1fee12ba2abf8ae4d15eb768e91f5da254367440
zouhair-bit <64888405+zouhair-bit@users.noreply.github.com>
2023-01-21 16:58:10 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,19 +1,30 @@
 from pyfiles.list import list
-
+from pyfiles.edit import edit
 help = '''commands:
         - test: executes a test
         - list: choose a folder and see whats inside of it - 'self' to see existing folder 
         - exit: stops the program
         '''
+art = """
+        __________
+        |^|     | |
+        | |_____| |
+        |  _____  |
+        | |     | |
+        | |_____| |
+        |_|_____|_|
+    """
+print(art)
 
 running = True
-
 while running:
     command = input("〉")
     if command == "test":
         print("hi test test")
     elif command == "list":
         print(list())
+    elif command == "edit":
+        print(edit())
     elif command == "help":
         print(help)
     elif command == "exit":
--- pyfiles/list.py
+++ pyfiles/list.py
@@ -2,10 +2,8 @@ import os
 
 def list():
     while True:
+            print(os.listdir("folders")) 
             direct_name = input("Please input directory name that you need to list:")
-            if direct_name == "self":
-                directory = os.listdir("folders")
-                return(directory)
             try:
                 directory = os.listdir("folders/"+direct_name)
                 return(directory)
